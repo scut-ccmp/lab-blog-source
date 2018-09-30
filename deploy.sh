d905879dba4898dc2c8e698020d53d67cc8f48c6
Jason.Yu <morty.yu@yahoo.com>
2018-09-30 12:42:35 +0800

    deploy.sh rm xml

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 find ./public -type f -name '*.html' -exec rm {} \;
+find ./public -type f -name '*.xml' -exec rm {} \;
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
